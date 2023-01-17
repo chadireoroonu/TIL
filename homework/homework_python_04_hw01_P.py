883ec4efd9974235913c33cf38c06e57a4fa597e
chadireoroonu <chadireoroonu@gmail.com>
2023-01-17 21:16:14 +0900

    01172_2116

--- homework/homework_python_04_hw01_P.py
+++ homework/homework_python_04_hw01_P.py
@@ -1,19 +1,19 @@
-words = ["round" , "drea", "magnet" , "tweet" , "tweet", "trick", "kiwi"]
+words = ["round" , "dream", "magnet" , "tweet" , "tweet", "trick", "kiwi"]
 
 i = 0
-alpha = []
-while True:
-    if words[i][-1] != words[i+1][0]:
-        break
+alpha = [] # 새로운 것들을 추가해줄 빈 리스트 생성
+while i < len(words) -1: # 범위가 넘어가는 것을 막기 위해 words의 길이 -1로 길이 조정
+    if words[i][-1] != words[i+1][0]: # 단어의 마지막 글자와 다음 단어의 시작 글자가 다르다면 탈락
+        print(f'{i+2} lose')
     else:
-        if words[i] in alpha:
-            break
+        if words[i] in alpha: # 이미 리스트에 있는 단어를 말하면 탈락
+            print(f'{i+1} lose')
         else:
-            alpha.append(words[i])
+            alpha.append(words[i]) #리스트에 없는 단어는 추후 검사를 위해 담아줌
             pass
     i += 1
 
-print(f'{i+2} lose')
+# 5 lose
 
 
 # for i in words:

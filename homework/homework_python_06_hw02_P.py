dc7a3c14bd9a5d1c2c4d325f1240bfd3b638c568
chadireoroonu <chadireoroonu@gmail.com>
2023-01-18 22:47:32 +0900

    01183_2247

---
+++ homework/homework_python_06_hw02_P.py
@@ -0,0 +1,26 @@
+# A.    입력 예시 
+# ['eat','tea','tan','ate','nat','bat']
+
+# B.    출력 예시 
+# [ ['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat'] ] 
+
+# words = input()
+# word1 = []
+# word2 = []
+# def word in words:
+#     for eat[i] in range(eat):
+#         if True:
+#             word1.append(words)
+#         elif:
+#             for tan[i] in range(tan):
+#                 if True:
+#                     word2.append(words)
+#                 else:
+
+words = input()
+def group_anagams(words):
+    for range(words)[i] in words:
+        [].append(words)
+    return []
+
+print([])

2ed0ef3e13cd15331d29b28b96ffd0f5cf19009b
chadireoroonu <chadireoroonu@gmail.com>
2023-01-19 17:51:36 +0900

    01194_1751

--- homework/homework_python_05_hw02_P.py
+++ homework/homework_python_05_hw02_P.py
@@ -1,3 +1,14 @@
 # fn_d(91) 
 # 출력 예시 
 # 101
+
+n = input()
+
+num_list = []
+def fn_d(n):
+    for n[i] in range(len(n)):
+        num_list.append(n[i])
+    return (num_list)
+
+print(fn_d(n))
+    

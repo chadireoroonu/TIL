edc5ccaa89d0d29c2194c25f0607556da4a25c5f
chadireoroonu <chadireoroonu@gmail.com>
2023-01-23 21:19:25 +0900

    01231_2119

--- workshop/03_python_workshop.md
+++ workshop/03_python_workshop.md
@@ -1,69 +1,74 @@
-# WorkShop
+# 세로로 출력하기
+자연수 number를 입력 받아, 1부터 number 까지의 수를 세로로 한줄씩 출력하시오.
 
-### 세로로 출력하기
 
-- 자연수 number를 입력 받아, 1부터 number까지의 수를 세로로 한줄씩 출력하시오.
-  
 number = int(input())
-range_number = range(1, number+1) #range는 순서가 있는 시퀀스며, 변형불가능한 특징
 
-for num in range_number:
-    print(num)
+for i in range(1, number+1):
+    print(i)
 
-### 가로로 출력하기
 
-* 자연수 number를 입력 받아, 1부터 number까지의 수를 가로로 한칸씩 띄어 출력하시오.
-  
+# 가로로 출력하기
+자연수 number를 입력 받아, 1부터 number까지의 수를 가로로 한칸씩 띄어 출력하시오.
+
 number = int(input())
-for num in range(1, number+1):
-    print(num, end=' ')
 
-### 거꾸로 세로로 출력하기
+for i in range(1, number+1):
+    print(i, end=' ')
 
-- 자연수 number를 입력 받아, number부터 0까지의 수를 세로로 한줄씩 출력하시오.
-  
+
+# 거꾸로 세로로 출력하기
+자연수 number를 입력 받아, number부터 0까지의 수를 세로로 한줄씩 출력하시오.
+
 number = int(input())
-for num in range(number, -1, -1): # range 세 번째 자리는 step인자
-    print(num)
+for i in range(number, -1, -1):
+    print(i)
 
-### 거꾸로 출력해 보아요 (SWEA #1545)
 
-* 자연수 number를 입력 받아, number부터 0까지의 수를 가로로 한칸씩 띄어 출력하시오.
-  
+# 거꾸로 출력해 보아요
+자연수 number를 입력 받아, number부터 0까지의 수를 가로로 한칸씩 띄어 출력하시오.
+
 number = int(input())
-for num in range(number, -1, -1):
-    print(num, end=' ')
+for i in range(number, -1, -1):
+    print(i, end= ' ')
 
-### N줄 덧셈 (SWEA #2025)
 
-- 입력으로 자연수 number가 주어질 때, 1부터 주어진 자연수 number까지를 모두 더한
-  값을 출력하시오. 단, 주어지는 숫자는 10000을 넘지 않는다. 예를 들어, 주어진 숫자가
-  10일 경우 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55이므로, 출력해야 할 값은 55이다.
-  
+# N줄 덧셈
+입력으로 자연수 number가 주어질 때, 1부터 주어진 자연수 number까지를 모두 더한 값을 출력하시오.
+단, 주어지는 숫자는 10000을 넘지 않는다. 예를 들어, 주어진 숫자가 10일 경우 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55이므로, 출력해야 할 값은 55이다.
+
 number = int(input())
-result = 0 # 값을 초기화하지 않기 위해 반복문 위에 선언
-for num in range(1, number+1):
-    result += num
+result = 0
+for i in range(1, number+1):
+    result += i
+print(result)
 
-print(result) # for 안에 print를 넣으면 결과값을 계속 출력
 
-### 삼각형 출력하기
+# 삼각형 출력하기
+자연수 number를 입력 받아, 아래와 같이 높이가 number인 삼각형을 출력하시오.
 
-* 자연수 number를 입력 받아, 아래와 같이 높이가 number인 삼각형을 출력하시오.
-  
 number = int(input())
-for num in range(1, number+1):
-    print(' '*(number-num)+'*'*num)
+for i in range(1, number+1):
+    print(' '*(number-i)+('*'*i))
 
-### 중간값 찾기 (SWEA #2063 변형)
 
-- 중간값은 통계 집단의 수치를 크기 순으로 배열 했을 때 전체의 중앙에 위치하는 수치를
-  뜻한다. 리스트 numbers에 입력된 숫자에서 중간값을 출력하라.
-  
-  ```python
-  numbers = [
-  85, 72, 38, 80, 69, 65, 68, 96, 22, 49, 67,
-  51, 61, 63, 87, 66, 24, 80, 83, 71, 60, 64,
-  52, 90, 60, 49, 31, 23, 99, 94, 11, 25, 24,
-  ]
-  ```
+# 중간값 찾기
+중간값은 통계 집단의 수치를 크기 순으로 배열 했을 때 전체의 중앙에 위치하는 수치를 뜻한다.
+리스트 numbers에 입력된 숫자에서 중간값을 출력하라.
+
+numbers = [
+    85, 72, 38, 80, 69, 65, 68, 96, 22, 49, 67,
+    51, 61, 63, 87, 66, 24, 80, 83, 71, 60, 64,
+    52, 90, 60, 49, 31, 23, 99, 94, 11, 25, 24,
+]
+
+num = []
+for i in range(len(numbers)):
+    min = 100
+    for i in range(len(numbers)):
+        if numbers[i] < min:
+            min = numbers[i]
+    num.append(min)
+    numbers.remove(min)
+
+print(num[len(num)//2])
--- workshop/03_python_workshop.py
+++ workshop/03_python_workshop.py
@@ -1,122 +1,51 @@
-# number = int(input())
-# range_number = range(1, number+1) #range는 순서가 있는 시퀀스며, 변형불가능한 특징
+# 1번 문제
+number = int(input())
 
-# for num in range_number:
-#     print(num)
+for i in range(1, number+1):
+    print(i)
 
+# 2번 문제
+number = int(input())
 
+for i in range(1, number+1):
+    print(i, end=' ')
 
-# number = int(input())
-# for num in range(1, number+1):
-#     print(num, end=' ')
+# 3번 문제
+number = int(input())
+for i in range(number, -1, -1):
+    print(i)
 
+# 4번 문제
+number = int(input())
+for i in range(number, -1, -1):
+    print(i, end= ' ')
 
+# 5번 문제
+number = int(input())
+result = 0
+for i in range(1, number+1):
+    result += i
+print(result)
 
-# number = int(input())
-# for num in range(number, -1, -1): # range 세 번째 자리는 step인자
-#     print(num)
+# 6번 문제
+number = int(input())
+for i in range(1, number+1):
+    print(' '*(number-i)+('*'*i))
 
+# 7번 문제
+numbers = [
+    85, 72, 38, 80, 69, 65, 68, 96, 22, 49, 67,
+    51, 61, 63, 87, 66, 24, 80, 83, 71, 60, 64,
+    52, 90, 60, 49, 31, 23, 99, 94, 11, 25, 24,
+]
 
+num = []
+for i in range(len(numbers)):
+    min = 100
+    for i in range(len(numbers)):
+        if numbers[i] < min:
+            min = numbers[i]
+    num.append(min)
+    numbers.remove(min)
 
-# number = int(input())
-# for num in range(number, -1, -1):
-#     print(num, end=' ')
-
-
-
-# number = int(input())
-# result = 0 # 값을 초기화하지 않기 위해 반복문 위에 선언
-# for num in range(1, number+1):
-#     result += num
-
-# print(result) # for 안에 print를 넣으면 결과값을 계속 출력
-
-
-# solution1
-# number = int(input())
-# for num in range(1, number+1):
-#     print(' '*(number-num)+'*'*num)
-
-
-# number = int(input())
-# for num in range(number):
-#     print(num, '첫 번째 반복문')
-#     for num in range(number, -1, -1):
-#         print(num, '두 번째 반복문')
-#     print()
-
-
-# solutuin2
-# number = int(input())
-# for star in range(number+1):
-#     for space in range(number-star-1, -1, -1):
-#         print(' ', end='')
-#     print('*'*star)
-
-
-# number = int(input())
-# for i in range(1, number+1):
-#     print(i)
-#     for j in range(1, number+1):
-#         print(i, j)
-
-# arr = [[i for i in range(4)] for j in range(4)]
-# print(arr)
-
-# arr = [
-#     [1, 2, 3, 4],
-#     [5, 6, 7, 8],
-#     [9, 10, 11, 12],
-#     [13, 14, 15, 16],
-#     [17, 18, 19, 20]
-# ]
-
-# for i in range(len(arr)):
-#     for j in range(len(arr)):
-#         # print(i, j) # 프린트 값은 각 숫자들의 좌표라고 생각
-#         print(arr[i][j])
-
-# 집에가서 순서 다양하게 뽑아보기
-# 뒤에서부터, 세로로, 세로 거꾸로 등
-# 파이썬 튜터 사이트로도 확인해보기
-# 리스트 가로세로 길이가 다르게도 출력
-
-# arr = [
-#     [1, 2, 3, 4],
-#     [5, 6, 7, 8],
-#     [9, 10, 11, 12],
-#     [13, 14, 15, 16],
-#     [17, 18, 19, 20]
-# ]
-
-# for i in range(len(arr)):
-#     for j in range(len(arr[i])):
-#         # print(i, j) # 프린트 값은 각 숫자들의 좌표라고 생각
-#         print(arr[i][j])
-
-
-# numbers = [
-#     85, 72, 38, 80, 69, 65, 68, 96, 22, 49, 67,
-#     51, 61, 63, 87, 66, 24, 80, 83, 71, 60, 64,
-#     52, 90, 60, 49, 31, 23, 99, 94, 11, 25, 24
-# ]
-
-# num = []
-# for i in range(len(numbers)):
-#     for j in range(len(numbers)):
-#         if numbers[i] < numbers[j]:
-#             num.append(numbers[i])
-#         else 
-
-# print(num)
-
-
-# num = []
-# for i in len(numbers):
-#     for j in range(len(numbers)):
-#         if numbers[i] < numbers[j]:
-#             num.append(numbers[i])
-#         else 
-
-# print(num)
-
+print(num[len(num)//2])

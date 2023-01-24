7d19f820d7c6281f59a948e41786f344042647e3
chadireoroonu <chadireoroonu@gmail.com>
2023-01-24 13:56:12 +0900

    01242_1356

---
+++ workshop/04_python_workshop_2.py
@@ -0,0 +1,49 @@
+## 1번 문제
+# n = int(input())
+
+# num = []
+# for i in range(1, n+1):
+#     if n % i == 0:
+#         num.append(i)
+#     else:
+#         pass
+# num.sort()
+
+# print(num)
+
+
+## 2번 문제
+# num = eval(input())
+# def list_sum(num):
+#     sum = 0
+#     for i in num:
+#         sum += i
+#     return sum
+
+# print(list_sum(num))
+
+
+## 3번 문제
+# dict = eval(input())
+# def dict_list_sum(dict):
+#     sum = 0
+#     for i in range(len(dict)):
+#         sum += dict[i]['age']
+#     return sum
+
+# print(dict_list_sum(dict))
+
+
+## 4번 문제
+# num = eval(input())
+# def all_list_sum(num):
+#     sum = 0
+#     for i in range(len(num)):
+#         for j in range(len(num[i])):
+#             sum += num[i][j]
+#     return sum
+
+# print(all_list_sum(num))
+
+
+## 5번 문제

c4445c9f3d7e4e0e235d44568640cf30d7c54681
chadireoroonu <chadireoroonu@gmail.com>
2023-01-26 14:04:44 +0900

    01264_1404

---
+++ new.py
@@ -0,0 +1,15 @@
+# sample_list = [11, 22, 33, 55, 66]
+
+# three = sample_list.pop(3)
+# print(sample_list)
+# print(three)
+# sample_list.append(77)
+# print(sample_list)
+# sample_list.insert(2, three)
+# print(sample_list)
+
+my_tuple = (11, 22, 33, 44, 55, 66)
+
+new_tuple = my_tuple[3:5]
+print(new_tuple)
+print(my_tuple)

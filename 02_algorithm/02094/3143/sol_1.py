0e1ca55cf2285ba87662eb34f0efd3e2da9c1c49
chadireoroonu <chadireoroonu@gmail.com>
2023-02-09 17:49:39 +0900

    02094_1749

--- 02_algorithm/02094/3143/sol_1.py
+++ 02_algorithm/02094/3143/sol_1.py
@@ -4,4 +4,26 @@ sys.stdin = open('input.txt')
 t = int(input())
 
 for tc in range(1, t+1):
-    
+    a, b = list(map(str, input().split()))
+
+    real_a = a
+    count = 0
+    while True:
+        if b in a:
+            a = list(a)
+            for i in range(len(a) - len(b) + 1):
+                temp = ''
+                for j in range(len(b)):
+                    temp += a[i + j]
+                if temp == b:
+                    count += 1
+                    for k in temp:
+                        a.remove(k)
+                    if b not in a:
+                        break
+        else:
+            break
+
+
+    key = len(real_a) - len(b) * count + count
+    print(f'#{tc} {key}')
---
+++ 02_algorithm/02094/3143/sol_2.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('input.txt')
+
+t = int(input())
+for tc in range(1, t+1):
+    a, b = list(map(str, input().split()))
+
+

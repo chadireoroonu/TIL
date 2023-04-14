3464626c66c003045950e623f2caa4ad8f02e43b
chadireoroonu <chadireoroonu@gmail.com>
2023-04-14 22:37:51 +0900

    04145_2237

--- 02_algorithm/04145/11724_connected_graph/sol.py
+++ 02_algorithm/04145/11724_connected_graph/sol.py
@@ -6,12 +6,13 @@ load = [[0] * (n+1) for _ in range(n+1)]
 for _ in range(m):
     u, v = list(map(int, sys.stdin.readline().split()))
     load[u][v] = load[v][u] = 1
-connect = []
+connect = 0
 
+# DFS 몇 번 진행되는 지 파악
 for i in range(1, n+1):
     visited = [0] * (n+1)
     visited[i] = 1
-    temp = i
+    temp = [i]
     stack = [i]
     while stack:
         k = stack.pop()
@@ -20,9 +21,12 @@ for i in range(1, n+1):
                 stack.append(j)
                 visited[j] = 1
                 load[k][j] = load[j][k] = 0
-                temp += j
-    if temp > i:
-        connect.append(temp)
+                temp.append(j)
+    for j in range(len(temp)):
+        for k in range(len(temp)):
+            if load[temp[j]][temp[k]] or load[temp[k]][temp[j]]:
+                load[temp[j]][temp[k]] = load[temp[k]][temp[j]] = 0
+    if len(temp) > 1:
+        connect += 1
 
 print(connect)
-print(len(connect))

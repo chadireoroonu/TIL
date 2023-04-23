cffe9fa6b8c7f9c8cc50854d772db150e6210bae
chadireoroonu <chadireoroonu@gmail.com>
2023-04-23 23:51:28 +0900

    04237_2351

---
+++ project/007/04237/movies/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Actor, Movie, Review
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+# Create your views here.
+@api_view(['GET'])
+def actor_list(request):
+    actors = Actor.objects.all()
+    return Response

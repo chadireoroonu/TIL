2ae94ee303b4e7173ecc1e2ad0d1a5164ecedd7a
chadireoroonu <chadireoroonu@gmail.com>
2023-04-27 22:56:41 +0900

    04274_2256

--- 02_algorithm/04274/16236_baby_shark/sol.py
+++ 02_algorithm/04274/16236_baby_shark/sol.py
@@ -7,21 +7,15 @@
 #    거리가 가까운 물고기가 많다면 위, 왼쪽 우선순위
 # 5. 아기상어 최초 크기 2, 좌표 9, 물고기 크기 1~6
 
-# BFS 사용, 이전에는 시간초과가 났으니 deque 사용
-# 먹을 수 있는 물고기를 찾으면, 동일거리까지만 탐색
-# 동일 거리 물고기 deque 삽입 시, i좌표, j좌표 순으로 비교
-# 작은 값은 왼쪽, 큰 값은 오른쪽에 넣기
-# 먹는 물고기 수 temp 기록, temp == shark => shark + 1
-
 
 # 최초 상어 좌표 찾은 후 find 함수 실행
-
 # find 함수
 # BFS 탐색으로 거리 기록
-# 먹을 수 있는 물고기 발견 시 시간 기록, deque 삽입
-# 같은 시간까지만 물고기 조사, 물고기 i, j 좌표 우선 정렬
-# 물고기 먹고 상어 위치 재할당
-# 먹은 물고기 수, 상어 크기 등 고려 후 반복
+# 먹을 수 있는 물고기 발견 시 [시간, i, j] 리스트에 추가
+# 최소거리 초과시 조사 중단, 물고기 i, j 좌표 우선 정렬
+# 먹을 물고기가 있다면 먹고, 상어 위치 재할당
+# 먹은 물고기 수와 상어 크기 비교후 상어 성장 확인
+# 먹을 물고기가 없을 때까지 반복
 
 
 import sys
@@ -29,61 +23,55 @@ from collections import deque
 sys.stdin = open('input.txt')
 
 
-def find(x, y):
+def find(x, y, fish):
     global shark
-    eat = 0
-    sec = 0
-    while True:
-        visited = [[0] * n for _ in range(n)]
-        fish = deque([])
-        sea = deque([x, y])
-        visited[x][y] = 1
-        while sea:
-            i = sea.popleft()
-            j = sea.popleft()
-            if fish and fish[0][0] < visited[i][j]:    # 거리가 찾은 최소 거리보다 많으면 조사 그만
-                continue
-            di, dj = [-1, 1, 0, 0], [0, 0, -1, 1]
-            for k in range(4):
-                ni, nj = i + di[k], j + dj[k]
-                if 0 <= ni < n and 0 <= nj < n and visited[ni][nj] == 0:
-                    if arr[ni][nj] <= shark:  # 상어와 같으면 이동 가능
-                        visited[ni][nj] = visited[i][j] + 1
-                        sea.append(ni)
-                        sea.append(nj)
-                        if 0 < arr[ni][nj] < 7 and arr[ni][nj] < shark: # 먹이 발견 시 시간, 좌표 기록
-                            wait = [visited[i][j], ni, nj]
-                            if fish:
-                                if fish[0][0] > wait[0]:
-                                    fish.appendleft(wait)
-                                elif fish[0][0] == wait[0]:
-                                    if fish[0][1] > wait[1]:
-                                        fish.appendleft(wait)
-                                    elif fish[0][1] == wait[1] and fish[0][2] > wait[2]:
-                                        fish.appendleft(wait)
-                            else:
-                                fish.append(wait)
-        if fish:
-            sec += fish[0][0]
-            x, y = fish[0][1], fish[0][2]
-            arr[x][y] = 0
-            eat += 1
-            if eat == shark:
-                shark += 1
-                eat = 0
-
-        else:
-            return print(sec)
+    visited = [[0] * n for _ in range(n)]
+    load = deque([x, y])
+    visited[x][y] = 1
+    while load:
+        i = load.popleft()
+        j = load.popleft()
+        if fish and fish[0][0] < visited[i][j]:    # 최소 거리 초과 시 조사 중단
+            continue
+        di, dj = [-1, 1, 0, 0], [0, 0, -1, 1]
+        for k in range(4):
+            ni, nj = i + di[k], j + dj[k]
+            if 0 <= ni < n and 0 <= nj < n and visited[ni][nj] == 0:
+                if sea[ni][nj] <= shark:  # 상어와 같으면 이동 가능
+                    visited[ni][nj] = visited[i][j] + 1
+                    load.append(ni)
+                    load.append(nj)
+                    if 0 < sea[ni][nj] < 7 and sea[ni][nj] < shark: # 먹이 발견 시 시간, 좌표 기록
+                        fish.append([visited[i][j], ni, nj])
+    return fish.sort()
 
 
 n = int(sys.stdin.readline().strip())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+sea = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 shark = 2
-iidx = jidx = 0
+sec = 0    # 총 이동 시간
+eat = 0    # 먹은 물고기
 
+x = y = 0    # 상어찾기
 for a in range(n):
     for b in range(n):
-        if arr[a][b] == 9:
-            iidx, jidx = a, b
+        if sea[a][b] == 9:
+            sea[a][b] = 0
+            x, y = a, b
 
-find(iidx, jidx)
+while True:
+    food = []    # 먹이 물고기 담을 배열
+    find(x, y, food)
+
+    if food:    # 문제 조건에 따라 맨 앞 물고기 먹음
+        sec += food[0][0]
+        x, y = food[0][1], food[0][2]
+        sea[x][y] = 0
+        eat += 1
+        if eat == shark:    # 상어 성장 확인
+            eat = 0
+            shark += 1
+    else:    # 먹을 물고기가 없다면 반복 중단
+        break
+
+print(sec)

407302a97c8427d2403e25b290532c3525403868
chadireoroonu <chadireoroonu@gmail.com>
2023-05-01 00:08:50 +0900

    05011_0008

---
+++ 06_JS/99_homework/homework_2_2.js
@@ -0,0 +1,16 @@
+let input = 5
+
+let star = ''
+let space = ''
+
+for (let i = 1; i <= input; i++) {
+    for (let j = 0; j < 2*i-1; j++) {
+        star += '*'
+    }
+    for (let k = input - i - 1; k >= 0; k--) {
+        space += ' '
+    }
+    console.log(space+star)
+    star = ''
+    space = ''
+}

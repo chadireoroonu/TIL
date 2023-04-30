64b06827cf4e8d52afab6b72531145a3f39ede1e
chadireoroonu <chadireoroonu@gmail.com>
2023-05-01 00:08:26 +0900

    04307_2350

--- 02_algorithm/04307/1027_building/sol.py
+++ 02_algorithm/04307/1027_building/sol.py
@@ -1,17 +1,9 @@
 # 1. 모든 건물에 대해 좌, 우 방향 탐색
 # 2. 볼 수 있는 건물의 개수 세기
-#    발견한 옆 건물 높이 기록 buildings[i] > 건물 => h = 0
-#    1. 좌측탐색
-#       1. h == 0 and buildings[j] < buildings[i]
-#          기울기 : (buildings[i]-buildings[j]) / (i-j) 기록
-#          기록된 숫자보다 기울기가 작으면 temp += 1, 기울기 재할당
-#       2. h == 0 and buildings[j] > buildings[i]
-#          temp += 1, h = 1
-#          기울기 : (buildings[i]-buildings[j]) / (i-j) 기록
-#       3. h == 1 and buildings[j] > buildings[i]
-#          기록된 숫자보다 기울기가 작으면 temp += 1, 기울기 재할당
-#       4. h == 1 and buildings[j] < buildings[i]
-#          그냥 넘어가기
+#    1. 좌측탐색 -> 기울기 -
+#       기울기 : (buildings[i] - buildings[j]) / (i - j)
+#    2. 우측탐색 -> 기울기 +
+#       기울기 : (buildings[j] - buildings[i]) / (j - i)
 # 3. maxi 보다 크다면 재할당
 # 4. 최종 maxi 값 출력
 
@@ -24,52 +16,17 @@ maxi = 0
 
 for i in range(N):
     temp = 0
-    h, r = 0, 1000000001
-    for j in range(i-1, -1, -1):
-        if h:
-            if (buildings[i] - buildings[j]) / (i - j) < r:
-                r = (buildings[i] - buildings[j]) / (i - j)
-                temp += 1
-                print(f'i={i}, j={j}, r={r}')
-        else:
-            if buildings[j] < buildings[i] and (buildings[i] - buildings[j]) / (i - j) < r:
-                r = (buildings[i] - buildings[j]) / (i - j)
-                temp += 1
-                print(f'i={i}, j={j}, r={r}')
-            elif buildings[j] > buildings[i]:
-                r = (buildings[i] - buildings[j]) / (i - j)
-                temp += 1
-                h = 1
-                print(f'i={i}, j={j}, r={r}')
-            elif buildings[j] == buildings[i]:
-                r = 0
-                temp += 1
-                h = 1
-                print(f'i={i}, j={j}, r={r}')
-    h = r = 0
-    for j in range(i + 1, N):
-        if h:
-            if (buildings[j] - buildings[i]) / (j - i) > r:
-                r = (buildings[j] - buildings[i]) / (j - i)
-                temp += 1
-                print(f'i={i}, j={j}, r={r}')
-        else:
-            if buildings[j] < buildings[i] and (buildings[i] - buildings[j]) / (j - i) > r:
-                r = (buildings[j] - buildings[i]) / (j - i)
-                temp += 1
-                print(f'i={i}, j={j}, r={r}')
-            elif buildings[j] > buildings[i]:
-                r = (buildings[j] - buildings[i]) / (j - i)
-                temp += 1
-                h = 1
-                print(f'i={i}, j={j}, r={r}')
-            elif buildings[j] == buildings[i]:
-                r = 0
-                temp += 1
-                h = 1
-                print(f'i={i}, j={j}, r={r}')
+    r = 1000000001
+    for j in range(i-1, -1, -1):    # 좌측탐색
+        if (buildings[i] - buildings[j]) / (i - j) < r:
+            r = (buildings[i] - buildings[j]) / (i - j)
+            temp += 1
+    r = -1000000001
+    for j in range(i + 1, N):    # 우측탐색
+        if (buildings[j] - buildings[i]) / (j - i) > r:
+            r = (buildings[i] - buildings[j]) / (i - j)
+            temp += 1
 
-    print(f'i={i}, temp={temp}')
     if temp > maxi:
         maxi = temp
 

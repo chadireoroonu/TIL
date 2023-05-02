9d17a7b4d7d7990335ef11db5f48698f5aaa0f57
chadireoroonu <chadireoroonu@gmail.com>
2023-05-02 13:53:01 +0900

    05022_1352

---
+++ 07_vue/03_vue_cli/vue_cli/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- 사용 -->
+    <MyComponent />
+    <MyComponent />
+    <HelloWorld msg="반가워 Vue!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'  // 불러오기
+// .vue 생략 가능, 앞에 . 대신 @ 사용 시, src 폴더 의미
+// import 뒤 이름은 그냥 변수, App 내에서만 일치시켜주면 페이지 동작
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent,  // 등록
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 07_vue/03_vue_cli/vue_cli/src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild />
+    <MyChild />
+    <MyChild />
+    <MyChild />
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild'
+
+export default {
+  name: 'MyComponent', // 개발자도구 vue에서 볼 이름
+  components : {
+    MyChild,
+  }
+}
+</script>
+
+<style>
+.border {
+  border: solid;
+}
+</style>

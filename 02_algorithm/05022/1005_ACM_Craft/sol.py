6a24c3c43b4f0719f6fdc7bc2e41f5a32cc12199
chadireoroonu <chadireoroonu@gmail.com>
2023-05-03 14:31:14 +0900

    05033_1431

--- 02_algorithm/05022/1005_ACM_Craft/sol.py
+++ 02_algorithm/05022/1005_ACM_Craft/sol.py
@@ -1,53 +1,38 @@
-# 트리로 부모 정보 저장
-# BFS 탐색 total 저장
-# 시간 제한 방지 -> 도착 노드 도착 시, 출력 조건 설정
+# 1. 리스트로 먼저 건설되어야 하는 건물 정보 저장
+# 2. BFS 탐색, total 누적 시간 저장
+# 3. 먼저 건축되어야 하는 건물이 없을 경우 queue 추가, 건축 시작
 
 # 1차 시도 -> 4% 틀렸습니다.
+# 2차 시도 -> 시간 초과
 
 import sys
 from collections import deque
 sys.stdin = open('input.txt')
 
-def sol(queue):
-    while queue:
-        i = queue.popleft()
-        for j in child[i]:
-            # print(f'i={i}, j={j}')
-            total[j] = max(total[i] + time[j], total[j])
-            print(total)
-            queue.append(j)
-            print(f'deque={queue}')
-            count[j] -= 1
-            print(f'count={count}')
-            if not count[w-1]:
-                return print(total[w-1])
-    # return print(total[w-1])
-
 T = int(sys.stdin.readline().strip())
 for tc in range(T):
     N, K = map(int, sys.stdin.readline().split())
-    time = list(map(int, sys.stdin.readline().split()))
-    count = [0] * N
-    child = [[] for _ in range(N)]
+    time = list(map(int, sys.stdin.readline().split()))    # 건축 시간
+    count = [0] * N    # 먼저 건축되어야 하는 건물 수
+    child = [[] for _ in range(N)]    # 건축 완료 후 건설될 수 있는 건물들
     for _ in range(K):
         a, b = map(int, sys.stdin.readline().split())
-        child[a-1].append(b-1)
-        count[b-1] += 1
+        child[a-1].append(b-1)    # 건축 정보 저장
+        count[b-1] += 1    # 건축 조건 수 추가
     w = int(sys.stdin.readline().strip())
-    print(child)
-    print(f'count={count}')
-    # print(f'w={w}')
 
-    if count[w-1]:
-        total = time[::]
-        queue = deque([])
-        for x in range(N):
-            if not count[x]:
-                queue.append(x)
-                total[x] = time[x]
-        # print(f'time={time}')
-        print(f'first deque={queue}')
-        # print(f'first total={total}')
-        sol(queue)
-    else:
-        print(time[w-1])
+    total = time[::]
+    queue = deque()
+    for x in range(N):    # 건축 가능 시 queue 추가
+        if not count[x]:
+            queue.append(x)
+            total[x] = time[x]    # 누적시간 저장
+    while queue:
+        i = queue.popleft()
+        for j in child[i]:    # 건축 정보 리스트 활용
+            total[j] = max(total[i] + time[j], total[j])    # 큰 값 저장
+            count[j] -= 1    # 건축 조건 건물 수 조정
+            if not count[j]:    # 건축 가능 시 queue 추가
+                queue.append(j)
+
+    print(total[w-1])

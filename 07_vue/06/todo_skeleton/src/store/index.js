6064904b5aedc36af022a7f92e540d4093a1d96d
chadireoroonu <chadireoroonu@gmail.com>
2023-05-08 14:55:17 +0900

    05081_1455

---
+++ 07_vue/06/todo_skeleton/src/store/index.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    todos: [
+    ]
+  },
+  getters: {
+    allTodoCount(state) {
+      return state.todos.length
+    }
+  },
+  mutations: {
+    CREATE_TODO(state, todoItem) {
+      state.todos.push(todoItem)
+    },
+    DELETE_TODO(state, todoItem) {
+      const index = state.todos.indexOf(todoItem)
+      state.todos.splice(index, 1)
+    },
+    UPDATE_TODO(state, todoItem) {
+      state.todos = state.todos.map((todo) => {
+        if (todo === todoItem) { // state.todos에서 내가 클릭한 todo item 찾기 
+          todo.isCompleted = !todo.isCompleted // todo item의 isCompleted 뒤집기
+        }
+        return todo // 이거 빠트려서 고생!
+      })
+    },
+    LOAD_TODOS(state) {
+      const localStorageTodos = localStorage.getItem('todos')
+      const parsedTodos = JSON.parse(localStorageTodos)
+
+      state.todos = parsedTodos
+    }
+  },
+  actions: {
+    createTodo(context, todoTitle) {
+      // console.log(context)
+      const todoItem = {
+        title: todoTitle,
+        isCompleted: false
+      }
+      context.commit('CREATE_TODO', todoItem)
+      context.dispatch('saveTodosToLocalStorage')
+    },
+    deleteTodo(context, todoItem) {
+      context.commit('DELETE_TODO', todoItem)
+      context.dispatch('saveTodosToLocalStorage')
+    },
+    updateTodo(context, todoItem) {
+      context.commit('UPDATE_TODO', todoItem)
+      context.dispatch('saveTodosToLocalStorage')
+    },
+    saveTodosToLocalStorage(context) {
+      const jsonTodos = JSON.stringify(context.state.todos)
+      localStorage.setItem('todos', jsonTodos)
+    },
+    loadTodos(context) {
+      context.commit('LOAD_TODOS')
+    }
+  },
+  modules: {
+  }
+})

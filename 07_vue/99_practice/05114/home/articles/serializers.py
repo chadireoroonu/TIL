31fdde3f23792310347a470480f3f5ed5875c820
chadireoroonu <chadireoroonu@gmail.com>
2023-05-16 17:43:18 +0900

    05162_1743

--- 07_vue/99_practice/05114/home/articles/serializers.py
+++ 07_vue/99_practice/05114/home/articles/serializers.py
@@ -3,15 +3,15 @@ from .models import Article, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     # 어떤 article에 달릴 댓글인지 정보
-    article = serializers.PrimaryKeyRelatedField(read_only=True)
+    # article = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields =('article',)
 
 class ArticleSerializer(serializers.ModelSerializer):
     # comments를 추가한 정보 직렬화
     comments = CommentSerializer(many=True)
-
     class Meta:
         model = Article
         fields = '__all__'
